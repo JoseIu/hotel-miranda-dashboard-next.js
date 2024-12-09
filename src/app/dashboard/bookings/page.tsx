11c8a3f80e9ccfe44f0a51d37c98389fa8731eb0
Joselu <joseluissgz97@gmail.com>
2024-12-09 22:43:42 +0100

    feat: add Link to add new Booking

--- src/app/dashboard/bookings/page.tsx
+++ src/app/dashboard/bookings/page.tsx
@@ -1,15 +1,15 @@
 import { getBookings } from '@/app/actions/bookingAction';
 import { BookingsTable } from '@/components';
+import Link from 'next/link';
 
 const BookingsPage = async () => {
   const result = await getBookings();
   const bookings = result?.bookings ?? [];
 
-  console.log(bookings);
-
   return (
     <div>
       BookingsPage
+      <Link href={`/dashboard/bookings/id`}>New Booking</Link>
       <BookingsTable bookings={bookings} />
     </div>
   );
