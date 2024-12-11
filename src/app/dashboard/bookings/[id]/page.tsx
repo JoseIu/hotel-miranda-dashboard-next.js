11d26a65e2ded8a97ece1a90184e60a6050c8317
Joselu <joseluissgz97@gmail.com>
2024-12-11 11:14:38 +0100

    feat: add zod and react-hook-for to booking form

--- src/app/dashboard/bookings/[id]/page.tsx
+++ src/app/dashboard/bookings/[id]/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { checkAvailability } from '@/app/actions/bookings/checkAvailability';
-import { RoomType } from '@/interfaces/roomResp';
+import { DatePicker, InputForm } from '@/components';
+import { RoomAvailability, RoomType } from '@/interfaces/roomResp';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { use, useEffect, useState } from 'react';
+import { SubmitHandler, useForm, useWatch } from 'react-hook-form';
+import { BookingSchema, bookingShema } from './zod/bookingSchema';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -10,46 +14,83 @@ type Props = {
 const BookingFormPage = ({ params }: Props) => {
   const { id } = use(params);
 
-  const [date, setDate] = useState({
-    start: '',
-    end: '',
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<BookingSchema>({
+    resolver: zodResolver(bookingShema),
   });
+
+  const checkIn = useWatch({ control, name: 'checkin' });
+  const checkOut = useWatch({ control, name: 'checkOut' });
+  const [roomsAvailable, setRoomsAvailable] = useState<RoomAvailability[]>([]);
+
+  console.log({ checkIn, checkOut });
 
   console.log({ id });
 
   const checkAvailabilityy = async (start: string, end: string, room_type: RoomType) => {
     if (!start || !end) return;
     const response = await checkAvailability(start, end, room_type);
+    if (response?.error) return;
 
-    console.log({ response });
+    setRoomsAvailable(response!.availableRooms);
+  };
+
+  const onHandleSubmit: SubmitHandler<BookingSchema> = (data) => {
+    console.log(data);
   };
 
   useEffect(() => {
-    if (!date.start || !date.end) return;
+    if (!checkIn || !checkOut) return;
 
-    checkAvailabilityy(date.start, date.end, 'SINGLE_BED');
-  }, [date.end, date.start]);
+    checkAvailabilityy(checkIn, checkOut, 'SINGLE_BED');
+  }, [checkIn, checkOut]);
 
   return (
     <div>
-      BookingFormPage
-      <div>
-        <label htmlFor="check_in">check in</label>
-        <input
-          type="date"
-          name="check_in"
-          id="check_in"
-          onChange={(ev) => setDate((prev) => ({ ...prev, start: ev.target.value }))}
-        />
+      <h2>BookingFormPage</h2>
+      <form onSubmit={handleSubmit(onHandleSubmit)}>
+        <div>
+          <InputForm
+            label="Name"
+            id="name"
+            error={errors['name']}
+            placeholder="Write guest name..."
+            {...register('name')}
+          />
+          <InputForm
+            label="Last name"
+            id="lastName"
+            error={errors['lastName']}
+            placeholder="Write last name guest..."
+            {...register('lastName')}
+          />
+        </div>
 
-        <label htmlFor="check_out">check out</label>
-        <input
-          type="date"
-          name="check_out"
-          id="check_out"
-          onChange={(ev) => setDate((prev) => ({ ...prev, end: ev.target.value }))}
-        />
-      </div>
+        <div>
+          <DatePicker label="Check in" id="check-in" error={errors['checkin']} {...register('checkin')} />
+          <DatePicker label="Check out" id="check-out" error={errors['checkOut']} {...register('checkOut')} />
+        </div>
+
+        <div>
+          <label htmlFor="">Rooms Available</label>
+          <select name="" id="">
+            {!roomsAvailable.length && (
+              <option value="">Select a check in abd Check out to get rooms availabe </option>
+            )}
+            {roomsAvailable.map((room) => (
+              <option key={room.id} value={`${room.room_number}-${room.room_type}`}>
+                {room.room_number} - {room.room_type}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
