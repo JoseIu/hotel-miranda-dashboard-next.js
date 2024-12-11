7fbb5d8a51ba5146f8d0d7b6bd5fede8ddb62108
Joselu <joseluissgz97@gmail.com>
2024-12-11 11:14:57 +0100

    feat: getAllRooms on RoomsPage

--- src/app/dashboard/rooms/page.tsx
+++ src/app/dashboard/rooms/page.tsx
@@ -1,4 +1,8 @@
-const RoomsPage = () => {
+import { getAllRooms } from '@/app/actions/rooms/getAllRooms';
+
+const RoomsPage = async () => {
+  const rooms = await getAllRooms();
+  console.log(rooms);
   return <div>RoomsPage</div>;
 };
 
