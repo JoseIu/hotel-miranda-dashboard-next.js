d46a4f3ba3898016d4971ff9edf609f6fc935347
Joselu <joseluissgz97@gmail.com>
2024-12-16 17:13:59 +0100

    feat: add Icon to SideBarItem

--- src/components/ui/sidebar/SideBarItem.tsx
+++ src/components/ui/sidebar/SideBarItem.tsx
@@ -1,17 +1,20 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import React from 'react';
 
 type Props = {
   path: string;
   title: string;
+  Icon: React.ElementType;
 };
-export const SideBarItem = ({ path, title }: Props) => {
+export const SideBarItem = ({ path, title, Icon }: Props) => {
   const menuPath = usePathname();
 
   const isActive = menuPath === path && 'active';
   return (
     <li className="sidebar-item">
       <Link className={`sidebar-item__link ${isActive}`} href={path}>
+        <Icon size={18} className="sidebar-item__icon" />
         {title}
       </Link>
     </li>
