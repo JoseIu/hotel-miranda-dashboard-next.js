1e1c09c582a5d93e9cf1dac09351d916eb40153c
Joselu <joseluissgz97@gmail.com>
2024-12-17 15:15:16 +0100

    feat: add sortBookings constant and sortBookings with getOrderBy

---
+++ src/constants/sortBookings.ts
@@ -0,0 +1,18 @@
+export const sortBookings = [
+  {
+    label: 'Guest',
+    value: 'guest',
+  },
+  {
+    label: 'Order date',
+    value: 'order-date',
+  },
+  {
+    label: 'Check in',
+    value: 'CHECK_IN',
+  },
+  {
+    label: 'Check out',
+    value: 'CHECK_OUT',
+  },
+];
