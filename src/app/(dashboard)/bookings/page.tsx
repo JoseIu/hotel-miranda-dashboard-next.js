b7b9e97cf78c2606dddf451310d968945a9f0089
Joselu <joseluissgz97@gmail.com>
2024-12-17 15:11:30 +0100

    feat: refacotor booking page, moving getBooking actions to BookingContent with suspence and add bookingsFilters component

--- src/app/(dashboard)/bookings/page.tsx
+++ src/app/(dashboard)/bookings/page.tsx
@@ -1,17 +1,37 @@
-import { getBookings } from '@/app/actions/bookingAction';
-import { BookingsTable } from '@/components';
+import { BookingContent } from '@/components/bookings/booking-content/BookingContent';
+import { BookingsFilters } from '@/components/bookings/booking-filters/BookingsFilters';
+import { TableSkeleton } from '@/components/ui/table-skeleton/TableSkeleton';
+import { BookingStatus } from '@/interfaces';
+import { PlusIcon } from '@primer/octicons-react';
 import Link from 'next/link';
+import { Suspense } from 'react';
 import './bookingsPage.scss';
+type Props = {
+  searchParams: Promise<{
+    search?: string;
+    status?: BookingStatus;
+    sort?: string;
+  }>;
+};
 
-const BookingsPage = async () => {
-  const result = await getBookings();
-  const bookings = result?.bookings ?? [];
+const BookingsPage = async ({ searchParams }: Props) => {
+  const search = (await searchParams).search || '';
+  const status = (await searchParams).status;
+  const sortBy = (await searchParams).sort;
 
   return (
-    <div className="sections">
-      BookingsPage
-      <Link href={`/bookings/new`}>New Booking</Link>
-      <BookingsTable bookings={bookings} />
+    <div className="sections bookings-page">
+      <div className="bookings-page__header">
+        <BookingsFilters />
+        <Link className="booking-add" href={`/bookings/new`}>
+          New Booking
+          <PlusIcon className="booking-add__icon" size={20} />
+        </Link>
+      </div>
+
+      <Suspense key={search + status + sortBy} fallback={<TableSkeleton />}>
+        <BookingContent search={search} status={status} orderBy={sortBy} />
+      </Suspense>
     </div>
   );
 };
