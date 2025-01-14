fbcf98a113bae8e506fe92480eb99eeccbcf53b1
Joselu <joseluissgz97@gmail.com>
2025-01-14 11:01:17 +0100

    feat: add getUsers server action and user interfae

---
+++ src/app/actions/users/getUsers.ts
@@ -0,0 +1,45 @@
+'use server';
+import prisma from '@/lib/prisma';
+
+type Props = {
+  page?: number;
+  take?: number;
+  search?: string;
+  status?: string;
+};
+
+export const getUsers = async ({ page = 1, take = 10, search, status }: Props) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const users = await prisma.user.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      where: {
+        name: {
+          contains: search,
+        },
+        status: {
+          equals: status === 'active' ? true : false,
+        },
+      },
+    });
+
+    return {
+      users: users,
+      error: false,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        users: [],
+        error: false,
+      };
+    }
+    return {
+      users: [],
+      error: false,
+    };
+  }
+};
---
+++ src/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  name: string;
+  created_at: Date;
+  email: string;
+  phone: string;
+  status: boolean;
+  jog_description: string;
+  user_image: string;
+}
