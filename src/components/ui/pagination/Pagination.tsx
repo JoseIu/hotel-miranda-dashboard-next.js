8c50316699e943d000cb7b73c4519be12c18ceae
Joselu <joseluissgz97@gmail.com>
2025-01-16 22:35:58 +0100

    feat: add Pagination component and link active

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -2,6 +2,8 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@primer/octicons-react';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
+import { generatePaginationNumbers } from './generatePaginationNumbers';
+import './pagination.scss';
 
 type Props = {
   totalPages: number;
@@ -12,13 +14,15 @@ export const Pagination = ({ totalPages }: Props) => {
   const serachParams = useSearchParams();
   const currentPage = Number(serachParams.get('page')) ?? 1;
 
+  const allPages = generatePaginationNumbers({ currentPage, totalPages });
+
   const createPageUrl = (pageNumber: number | string) => {
     const params = new URLSearchParams(serachParams);
     if (pageNumber === '...') {
       return `${pathName}?${params.toString()}`;
     }
 
-    if (+pageNumber === 0) {
+    if (+pageNumber <= 0) {
       return `${pathName}`;
     }
     if (+pageNumber > totalPages) {
@@ -29,18 +33,25 @@ export const Pagination = ({ totalPages }: Props) => {
     return `${pathName}?${params.toString()}`;
   };
   return (
-    <div>
-      <ul>
+    <div className="pagination">
+      <ul className="pagination__list">
         <li>
           <Link href={createPageUrl(currentPage - 1)}>
             <ChevronLeftIcon size={30} />
           </Link>
         </li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
+
+        {allPages.map((page, index) => (
+          <li key={page + '-' + index} className="pagination__list-item">
+            <Link
+              className={`pagination__list-link ${currentPage === page && 'pagination__list-link--active'}`}
+              href={createPageUrl(page)}
+            >
+              {page}
+            </Link>
+          </li>
+        ))}
+
         <li>
           <Link href={createPageUrl(currentPage + 1)}>
             <ChevronRightIcon size={30} />
