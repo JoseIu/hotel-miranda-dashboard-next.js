f5fb294cce47e98241f143ffb2aec2778da540c6
Joselu <joseluissgz97@gmail.com>
2025-01-16 22:35:01 +0100

    feat: update add Pagination component and add page to getUsers

--- src/components/users/UserContent.tsx
+++ src/components/users/UserContent.tsx
@@ -1,13 +1,23 @@
 import { getUsers } from '@/app/actions/users/getUsers';
+import { Pagination } from '../ui/pagination/Pagination';
 import { UserTable } from './user-table/UserTable';
 
 type Props = {
   search: string;
   status: string;
+  page: number;
 };
 
-export const UserContent = async ({ search, status }: Props) => {
-  const usersList = await getUsers({ search, status });
+export const UserContent = async ({ search, status, page }: Props) => {
+  const usersList = await getUsers({ search, status, page });
 
-  return <UserTable user={usersList.users} />;
+  // if (!usersList.users.length) redirect('/users');
+
+  return (
+    <>
+      <UserTable user={usersList.users} />
+
+      <Pagination totalPages={usersList.totalPages!} />
+    </>
+  );
 };
