a307d7df9113f586a1c4bd113e261dec6897a651
Joselu <joseluissgz97@gmail.com>
2025-01-17 10:55:43 +0100

    feat: add Pagination Component

--- src/components/rooms/room-table/RoomContent.tsx
+++ src/components/rooms/room-table/RoomContent.tsx
@@ -1,9 +1,16 @@
 import { getAllRooms } from '@/app/actions/rooms/getAllRooms';
+import { Pagination } from '@/components/ui/pagination/Pagination';
 import { RoomTable } from './RoomTable';
 type Props = {
   filter: string;
+  page: number;
 };
-export const RoomContent = async ({ filter }: Props) => {
-  const rooms = await getAllRooms({ filter });
-  return <RoomTable rooms={rooms.rooms} />;
+export const RoomContent = async ({ filter, page }: Props) => {
+  const { rooms, totalPages } = await getAllRooms({ filter, page });
+  return (
+    <>
+      <RoomTable rooms={rooms} />
+      <Pagination totalPages={totalPages} />
+    </>
+  );
 };
