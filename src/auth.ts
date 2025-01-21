132b186e78556bcb8581b6f1a60e720cbb5f414f
Joselu <joseluissgz97@gmail.com>
2025-01-21 18:39:26 +0100

    fix: export handlers for API auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.cofig';
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
 });
